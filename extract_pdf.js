660eaf4a9efc18b4a130066444a318c9f600af8f
Xuan Diep <diepp031005@gmail.com>
2026-05-30 16:45:37 +0700

    chore: clean up redundant root src folder and old scripts

--- extract_pdf.js
+++
@@ -1,9 +0,0 @@
-const pdfParse = require('pdf-parse');
-const fs = require('fs');
-const path = require('path');
-
-const buf = fs.readFileSync(path.join(__dirname, 'fb_pages.pdf'));
-pdfParse(buf).then(d => {
-  fs.writeFileSync(path.join(__dirname, 'pdf_text.txt'), d.text);
-  console.log('Done, pages:', d.numpages);
-}).catch(e => console.error(e.message));
